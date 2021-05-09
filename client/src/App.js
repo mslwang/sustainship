56a57fa1fb4ca0c8ca269d7d046e893d06eb297f
Matthew Wang <mslwang2001@gmail.com>
2021-05-09 02:49:29 -0400

    Add basic submission page with routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import SubmissionPage from './pages/SubmissionPage';
+
+import { useMediaQuery } from 'react-responsive'
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/submission" exact component={SubmissionPage}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
