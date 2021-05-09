0f195433d863a6019ae2ff96aa5aca28880d492e
calvinl4 <calvinlin@live.ca>
2021-05-09 04:12:44 -0400

    oof

---
+++ client/src/actions/shipmentActions.js
@@ -0,0 +1,7 @@
+// const getShipmentsByAreaCode = (areaCode) => {
+//         axios.get('/api/shipments/' + areaCode)
+//         .then(res => {
+//             const shipmentsData = res.data;
+//             setShipments(shipmentsData)
+//         })
+//     }
